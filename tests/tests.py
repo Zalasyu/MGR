61d46c3cffb30ec9769f701800a66c5a4b4a54ec
mideenraj <rajm@oregonstate.edu>
2022-10-25 21:48:40 -0400

    Trying a modified absolute path

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 # Dependencies
 import os
 import unittest
-from ..MGR.src.data.PrepareInput import PrepareAudio
+from MGR.src.data.PrepareInput import PrepareAudio
 
 
 class PrepareAudioTestCase(unittest.TestCase):
