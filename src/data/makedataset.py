3c9b503eefce3f94a77742dda6d6fd2b3c8a4263
Alli <allisonglittle@gmail.com>
2022-10-26 18:11:01 -0700

    Added initial method to make training data. Does not yet include audio conversion.

--- src/data/makedataset.py
+++ src/data/makedataset.py
@@ -25,3 +25,24 @@ class MusicTrainingData:
         for g in os.listdir(path):
             self.genre_dict[g] = genre_count
             genre_count += 1
+
+    def make_training_data(self, path):
+        """
+        Creates numpy array of mel spectrograph and genre label using
+        the genre dictionary to create a one-hot vector. Processes
+        all files within genre-labeled directories at a specified path.
+        """
+        self.__create_genre_dictionary(path)
+        genre_count = len(self.genre_dict)
+        # Iterate through all genres
+        for genre in self.genre_dict:
+            # For each file in a genre
+            for f in os.listdir(path + "/" + genre):
+                # Use Librosa to create a spectrograph - Midhun's code
+                img = []
+                # Add image and label to training data
+                self.training_data.append([np.array(img), np.eye(genre_count)[self.genre_dict[genre]]])
+
+        # Shuffle and save dataset
+        np.random.shuffle(self.training_data)
+        np.save("training_data.npy", self.training_data)
