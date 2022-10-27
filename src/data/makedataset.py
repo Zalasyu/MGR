ef8900795b90ede3fa98855cf1cd7e9b5f9854c1
Alli <allisonglittle@gmail.com>
2022-10-26 18:05:26 -0700

    Added method to create genre dictionary

--- src/data/makedataset.py
+++ src/data/makedataset.py
@@ -1,9 +1,27 @@
+import os
+import numpy as np
+
+
 class MusicTrainingData:
-    """The MusicTrainingData class generates a training dataset
+    """
+    The MusicTrainingData class generates a training dataset
     to be used on the neural network. This will output a NumPy
     array file (.npy) that contains a mel spectrograph and a
     label for the spectrograph as a one-hot vector to signal
-    the correct genre."""
+    the correct genre.
+    """
 
     training_data = []
     genre_dict = {}
+
+    def __create_genre_dictionary(self, path):
+        """
+        Creates a dictionary using the names of the objects at a
+        specified path. The objects should be directories that
+        contain data for a single music genre (ex. Rap, Classical).
+        """
+        self.genre_dict = {}
+        genre_count = 0
+        for g in os.listdir(path):
+            self.genre_dict[g] = genre_count
+            genre_count += 1
