feec966851ddad20f6858db91472c6d41f0f0e35
Zalasyu <alec_moldovan@yahoo.com>
2022-10-26 20:25:31 -0500

    Fixed first test case ffor checking for mp3 files

--- tests/tests_MGR.py
+++ tests/tests_MGR.py
@@ -7,8 +7,9 @@ from src.data.PrepareInput import PrepareAudio as pa
 class TestPrepareAudio():
     """Tests the PrepareAudio class in src/data
     """
+    pa = pa()
 
     def test_mp3_file_accepted(self):
         """Test that a valid mp3 file is accepted"""
-        file = Path('tests/test_data/validfile_1.mp3')
-        assert pa.check_file_type(file) is True  # type: ignore
+        file_path = 'tests/test_data/validfile_1.mp3'
+        assert self.pa.check_file_exists(file_path) is True
