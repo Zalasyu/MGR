b8d170e05179a756709ae155a710a34b04543525
Zalasyu <alec_moldovan@yahoo.com>
2022-10-26 20:01:21 -0500

    Fixed path in test case.

--- tests/tests_MGR.py
+++ tests/tests_MGR.py
@@ -10,5 +10,5 @@ class TestPrepareAudio():
 
     def test_mp3_file_accepted(self):
         """Test that a valid mp3 file is accepted"""
-        file = Path('tests/test_data/valid file_1.mp3')
+        file = Path('tests/test_data/validfile_1.mp3')
         assert pa.check_file_type(file) is True  # type: ignore
