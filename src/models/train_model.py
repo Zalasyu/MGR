62b2eba7c0a547dbdc063f56ea86e57dd2c6b118
Jacob <jacobdmdumas@gmail.com>
2022-11-02 09:52:29 -0500

    added main.py and train_model.py for midpoint

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import numpy as np
 from tqdm import tqdm
 import torch
@@ -7,17 +5,14 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 
-import pandas as pd
-
-
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, classes):
         super().__init__() # just run the init of parent class (nn.Module)
         self.specrogram_width = 64
         self.specrogram_length = 2586
         # how many genres
-        self.classes = 5
+        self.classes = classes
         
         # making the 3 2D convolutional layers
         self.conv1 = nn.Conv2d(1, 32, 5) # input is 1 image, 32 convolutional features, 5x5 kernel / window size
@@ -25,20 +20,19 @@ class Net(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, 5)
 
         # changing convolutional layers to fully connected/linear layers (fc1 and fc2) by flattening
-        # input spectrogram size 640X480, 1 picture at a time
+        # input spectrogram size, 1 picture at a time
         x = torch.randn(self.specrogram_width, self.specrogram_length)
         x = torch.randn(self.specrogram_width, self.specrogram_length).view(-1, 1, self.specrogram_width, self.specrogram_length) 
         self._to_linear = None
         self.convs(x)        
         self.fc1 = nn.Linear(self._to_linear, 512) #flattening.
-        self.fc2 = nn.Linear(512, self.classes) # 512 in, 10 out bc we're doing 10 classes
+        self.fc2 = nn.Linear(512, self.classes) # 512 in, # of classes out
 
     def convs(self, x):
         # max pooling over 2x2, pooling is reducing the image size
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
-
         # print(x[0].shape)
 
         # setting the shape of the data
@@ -54,43 +48,29 @@ class Net(nn.Module):
         return F.softmax(x, dim=1)
 
 class Model():
-    def __init__(self):
-        """Constuctor method for TrainModel class can tweak epoch and batch_size"""
+    def __init__(self, batch_size, epochs, learning_rate, validation_percent, training_data):
+        """Constuctor method for TrainModel class can tweak epoch batch_size"""
 
-        #TODO add training data path
-        self.training_data = np.load("data/processed/training_data.npy", allow_pickle=True)
+        #add training data path
+        self.training_data = np.load(f"{training_data}", allow_pickle=True)
 
         # batch size is how many spectrographs it uses per pass
-        self.batch_size = 10
+        self.batch_size = batch_size
         
         # epochs are how many times it runs through all of the data
         # more epochs will make our model fit the training data more
-        self.epochs = 1
+        self.epochs = epochs
         
-        #TODO
-        # learning rate is?
-        self.learning_rate = 0.001
+        # learning rate
+        self.learning_rate = learning_rate
         
         # validation sample size
-        self.validation_percent = 0.3
+        self.validation_percent = validation_percent
 
-        # self.specrogram_width = 640
-        # self.specrogram_length = 480
         self.specrogram_width = 64
         self.specrogram_length = 2586
 
-    # def view_np(self):        
-    #     pd.set_option('display.max_rows', None)
-    #     pd.set_option('display.max_columns', None)
-    #     pd.set_option('display.width', None)
-    #     pd.set_option('display.max_colwidth', -1)
-    #     training_data = np.load("data/processed/training_data.npy", allow_pickle=True)
-    #     print(pd.DataFrame(training_data))
-
-
-    def train_model(self):
-        net = Net()
-
+    def train_model(self, classes):
         # if your computer is set up to train on the GPU it will use that, otherwise the CPU
         if torch.cuda.is_available():
             device = torch.device("cuda:0")  # 0 is the first GPU, can add more
@@ -99,7 +79,10 @@ class Model():
             device = torch.device("cpu")
             print("Running on the CPU")
 
-        optimizer = optim.Adam(net.parameters(), self.learning_rate)
+        # send to neural network to GPU
+        net = Net(classes).to(device)
+
+        optimizer = optim.Adam(net.parameters(), lr=self.learning_rate)
         loss_function = nn.MSELoss()
 
         # seprating training and validation data
@@ -110,7 +93,6 @@ class Model():
 
         # seperate out testing and validation data
         val_size = int(len(X)*self.validation_percent)
-        print("validation test size", val_size)
 
         # group data into train and test sets
         train_X = X[:-val_size]
@@ -118,33 +100,38 @@ class Model():
 
         test_X = X[-val_size:]
         test_y = y[-val_size:]
-        print(len(train_X), len(test_X), len(train_y), len(test_y))
+        #print(len(train_X), len(test_X), len(train_y), len(test_y))
+
 
         for epoch in range(self.epochs):
             # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
-            # print(f"{i}:{i+BATCH_SIZE}")
             for i in tqdm(range(0, len(train_X), self.batch_size)): 
+                #print(f"{i}:{i+self.batch_size}")
                 batch_X = train_X[i:i+self.batch_size].view(-1, 1, self.specrogram_width, self.specrogram_length)
                 batch_y = train_y[i:i+self.batch_size]
+                batch_X, batch_y = batch_X.to(device), batch_y.to(device)
 
                 net.zero_grad()
-
-                outputs = net(batch_X)
-                loss = loss_function(outputs, batch_y)
-                loss.backward()
-                optimizer.step()    # Does the update
+                outputs = net(batch_X) # pass in the reshaped batch
+                loss = loss_function(outputs, batch_y) # calc and grab the loss value
+                loss.backward()    # apply this loss backwards thru the network's parameters
+                optimizer.step()    # attempt to optimize weights to account for loss/gradients
 
             print(f"Epoch: {epoch}. Loss: {loss}")
 
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for i in tqdm(range(len(test_X))):
-                real_class = torch.argmax(test_y[i])
-                net_out = net(test_X[i].view(-1, 1, self.specrogram_width, self.specrogram_length))[0]  # returns a list, 
-                predicted_class = torch.argmax(net_out)
 
-                if predicted_class == real_class:
-                    correct += 1
-                total += 1
-        print("Accuracy: ", round(correct/total, 3))
+            # testing
+            correct = 0
+            total = 0
+            for i in tqdm(range(0, len(test_X), self.batch_size)):
+                batch_X = test_X[i:i+self.batch_size].view(-1, 1, self.specrogram_width, self.specrogram_length).to(device)
+                batch_y = test_y[i:i+self.batch_size].to(device)
+                batch_out = net(batch_X)
+
+                out_maxes = [torch.argmax(i) for i in batch_out]
+                target_maxes = [torch.argmax(i) for i in batch_y]
+                for i,j in zip(out_maxes, target_maxes):
+                    if i == j:
+                        correct += 1
+                    total += 1
+            print("Accuracy: ", round(correct/total, 3))
