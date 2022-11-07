05e3f2a747eeda79b692a84759845a6b4c586eee
mideenraj <rajm@oregonstate.edu>
2022-11-07 12:23:35 -0500

    Updating tests to match mains

--- tests/tests_MGR.py
+++ tests/tests_MGR.py
@@ -49,21 +49,11 @@ class TestPrepareAudio():
         file_path = 'tests/test_data/nonexistingFile.mp3'
         assert self.pa.check_file_exists(file_path) is False
 
-<<<<<<< HEAD
-    def test__file_converts_to_wav(self):
-        """Test if provided a valid path, convert_to_wav()
-        successfully convert the choosen file to .wav format."""
-        file_path = 'tests/test_data/validfile_1.mp3'
-        self.pa.convert_to_wav(file_path)
-        assert os.path.exists(
-            f"{self.pa.WAV_DATA_PATH}/validfile_1.wav") is True
-=======
     def test_array_data_type(self):
         """Test that the mel spectrogram array returned is of type list"""
         file_path = 'tests/test_data/validfile_2.wav'
         spectrogram_arr = self.pa.start(file_path)
         assert isinstance(spectrogram_arr, list)
->>>>>>> 61e201af9e2b1c732dc71b1016957139f53a3be9
 
 
 class TestTrainingData:
