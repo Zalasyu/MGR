0c181fa121e890f000638d68503289d8a6050e41
Zalasyu <alec_moldovan@yahoo.com>
2022-11-17 14:01:20 -0600

    Had an import error for the paths.

--- src/models/inference.py
+++ src/models/inference.py
@@ -19,13 +19,16 @@ def predict(model, input, target, class_mapping):
 
 
 if __name__ == "__main__":
+    ANNOTATIONS_FILE_CLOUD = "/nfs/stak/users/moldovaa/hpc-share/Data/features_30_sec.csv"
+    GENRES_DIR_CLOUD = "/nfs/stak/users/moldovaa/hpc-share/Data/genres_original"
+
     cnn = ConvoNetwork()
     state_dict = torch.load("CNN.pth")
     cnn = cnn.load_state_dict(state_dict)
 
     # LOAD gtzan dataset
-    gtzan = train.GtzanDataset(annotations_file=train.ANNOTATIONS_FILE_CLOUD,
-                               genres_dir=train.GENRES_DIR_CLOUD, device="cpu")
+    gtzan = train.GtzanDataset(annotations_file=ANNOTATIONS_FILE_CLOUD,
+                               genres_dir=GENRES_DIR_CLOUD, device="cpu")
 
     # Get a song from the dataset for inference
     input, target = gtzan[0][0], gtzan[0][1]
