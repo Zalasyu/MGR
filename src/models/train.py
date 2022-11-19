bc60db55f6c66e7b1c0681ed0f008449392f959b
Zalasyu <alec_moldovan@yahoo.com>
2022-11-19 14:36:08 -0600

    PRocess of saving model to correct path.

--- src/models/train.py
+++ src/models/train.py
@@ -9,6 +9,13 @@ import torchvision
 from torch.utils.tensorboard import SummaryWriter
 import datetime
 
+# Multi-GPU support
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+
 # TODO: Implement way to visualize training stage
 # TODO: Implement metrics
 # TODO: Implement a way to dynamically create initial weights for model
